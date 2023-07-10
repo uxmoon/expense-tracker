b060718ddd2f5e3b0053b19090872d28a2c244d7
uxmoon <uxmoon@gmail.com>
2023-07-10 10:01:37 -0300

    Update expenses category to test filter

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ function App() {
   const [selectedCategory, setSelectedCategory] = useState('')
   const [expenses, setExpenses] = useState([
     { id: 1, description: 'aaa', amount: 10, category: 'Utilities' },
-    { id: 2, description: 'bbb', amount: 20, category: 'Utilities' },
+    { id: 2, description: 'bbb', amount: 20, category: 'Groceries' },
     { id: 3, description: 'ccc', amount: 30, category: 'Utilities' },
-    { id: 4, description: 'ddd', amount: 40, category: 'Utilities' },
+    { id: 4, description: 'ddd', amount: 40, category: 'Entertainment' },
   ])
   const filteredExpenses = selectedCategory
     ? expenses.filter((e) => e.category === selectedCategory)
