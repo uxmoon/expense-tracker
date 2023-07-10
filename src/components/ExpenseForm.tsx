47d29d330c1011e88e1c669cfb7bd4dbd1f270df
uxmoon <uxmoon@gmail.com>
2023-07-10 11:07:20 -0300

    Add validation messages

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -4,9 +4,17 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import categories from '../categories'
 
 const schema = z.object({
-  description: z.string().min(3).max(50),
-  amount: z.number().min(0.01).max(100_000),
-  category: z.enum(categories),
+  description: z
+    .string()
+    .min(3, { message: 'Description should be at least 3 characters.' })
+    .max(50),
+  amount: z
+    .number({ invalid_type_error: 'Amount is required.' })
+    .min(0.01)
+    .max(100_000),
+  category: z.enum(categories, {
+    errorMap: () => ({ message: 'Category is required.' }),
+  }),
 })
 
 type ExpenseFormData = z.infer<typeof schema>
@@ -19,7 +27,7 @@ const ExpenseForm = () => {
   } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) })
 
   return (
-    <form action=''>
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
       <div className='mb-3'>
         <label htmlFor='description' className='form-label'>
           Description
@@ -39,7 +47,7 @@ const ExpenseForm = () => {
           Amount
         </label>
         <input
-          {...register('amount')}
+          {...register('amount', { valueAsNumber: true })}
           id='amount'
           type='number'
           className='form-control'
